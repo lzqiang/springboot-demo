5f7c1eceeabf1abe864f4fad56709db5cbcabdd7
liuzhq@tritonsfs.com <liuzhq@tritonsfs.com>
2018-11-03 11:29:28 +0800

    自动签到

---
+++ springbootservice/src/main/java/com/tritonsfs/springboot/schedule/CacSign.java
@@ -0,0 +1,86 @@
+package com.tritonsfs.springboot.schedule;
+
+import cn.hutool.http.HttpUtil;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author 2018/11/3 10:50 by 刘赵强
+ **/
+@Configuration
+@EnableScheduling
+@Component
+public class CacSign {
+    private static final Logger logger = LoggerFactory.getLogger(CacSign.class);
+    @Value("${cacLoginUrl}")
+    private String loginUrl;
+    @Value("${phone}")
+    private String phone;
+    @Value("${passowrd}")
+    private String password;
+    @Value("${cacSignUrl}")
+    private String cacSignUrl;
+
+    @Scheduled(cron = "0 0 8 * * ? ")
+    public void work() {
+        JSONObject jsonObject = null;
+        try {
+            jsonObject = loginCac();
+        } catch (Exception e) {
+            logger.error("登录失败,errorMsg={}", e.getMessage(), e);
+            return;
+        }
+        if (null != jsonObject) {
+            JSONObject model = jsonObject.getJSONObject("model");
+            String userId = model.getString("userId");
+            String loginToken = model.getString("loginToken");
+            if (StringUtils.isNotEmpty(userId) && StringUtils.isNotEmpty(loginToken)) {
+                try {
+                    sign(userId, loginToken);
+                } catch (Exception e) {
+                    logger.error("签到失败,errorMsg={}", e.getMessage(), e);
+                }
+            } else {
+                logger.error("userId或loginToken为空");
+            }
+        }
+    }
+
+    /**
+     * 登录
+     *
+     * @return
+     */
+    private JSONObject loginCac() {
+        Map<String, Object> paramsMap = new HashMap<>();
+        paramsMap.put("blackbox", "01");
+        paramsMap.put("device", "01");
+        paramsMap.put("passWord", password);
+        paramsMap.put("phoneNo", phone);
+        String body = HttpUtil.post(loginUrl, paramsMap);
+        return JSONObject.parseObject(body);
+    }
+
+    /**
+     * 签到
+     *
+     * @return
+     */
+    private JSONObject sign(String userId, String loginToken) {
+        Map<String, Object> paramsMap = new HashMap<>();
+        paramsMap.put("userId", userId);
+        paramsMap.put("loginToken", loginToken);
+        String body = HttpUtil.post(cacSignUrl, paramsMap);
+        return JSONObject.parseObject(body);
+    }
+}
